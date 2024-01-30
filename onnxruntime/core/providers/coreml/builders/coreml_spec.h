3f8430f139e39f8f00414b0f84731520fb0c44b8
Scott McKay <Scott.McKay@microsoft.com>
2024-01-31 09:43:12 +1000

    Add pragma to disable protobuf warning. Copied from ONNX source.

--- onnxruntime/core/providers/coreml/builders/coreml_spec.h
+++ onnxruntime/core/providers/coreml/builders/coreml_spec.h
@@ -3,8 +3,29 @@
 
 #pragma once
 
+#if defined(__GNUC__)
+#pragma GCC diagnostic push
+
+// Disable warning from protobuf code.
+//
+// In file included from coreml_proto/Model.pb.h:30:
+// In file included from _deps/protobuf-src/src/google/protobuf/extension_set.h:53:
+// _deps/protobuf-src/src/google/protobuf/parse_context.h:328:47:
+//     error: implicit conversion loses integer precision: 'long' to 'int' [-Werror,-Wshorten-64-to-32]
+#if defined(__has_warning)
+#if __has_warning("-Wshorten-64-to-32")
+#pragma GCC diagnostic ignored "-Wshorten-64-to-32"
+#endif
+#endif  // defined(__has_warning)
+
+#endif
+
 // Model.pb.h is generated in the build output directory from the CoreML protobuf files in
 // onnxruntime/core/providers/coreml/coremltools/mlmodel/format
 #include "coreml_proto/Model.pb.h"
+
+#if defined(__GNUC__)
+#pragma GCC diagnostic pop
+#endif
 
 namespace COREML_SPEC = CoreML::Specification;
