d50d266e24fa85219f2054e0484af25116dab55e
Scott McKay <Scott.McKay@microsoft.com>
2024-01-30 20:35:33 +1000

    Cleanups of makefiles

--- cmake/onnxruntime_providers_coreml.cmake
+++ cmake/onnxruntime_providers_coreml.cmake
@@ -20,50 +20,46 @@ else()
   set(_BUILD_COREMLTOOLS_MINIMAL ON)
 endif()
 
-set(_BUILD_COREML_PROTO ON)
-
 # Compile CoreML proto definition to ${CMAKE_CURRENT_BINARY_DIR}/coreml_proto
-if (_BUILD_COREML_PROTO)
-  set(COREML_PROTO_ROOT ${REPO_ROOT}/onnxruntime/core/providers/coreml/coremltools/mlmodel/format)
-  file(GLOB coreml_proto_srcs "${COREML_PROTO_ROOT}/*.proto")
+set(COREML_PROTO_ROOT ${REPO_ROOT}/onnxruntime/core/providers/coreml/coremltools/mlmodel/format)
+file(GLOB coreml_proto_srcs "${COREML_PROTO_ROOT}/*.proto")
 
-  onnxruntime_add_static_library(coreml_proto ${coreml_proto_srcs})
-  target_include_directories(coreml_proto
-                             PUBLIC $<TARGET_PROPERTY:${PROTOBUF_LIB},INTERFACE_INCLUDE_DIRECTORIES>
-                             "${CMAKE_CURRENT_BINARY_DIR}")
-  target_compile_definitions(coreml_proto
-                             PUBLIC $<TARGET_PROPERTY:${PROTOBUF_LIB},INTERFACE_COMPILE_DEFINITIONS>)
-  set_target_properties(coreml_proto PROPERTIES COMPILE_FLAGS "-fvisibility=hidden")
-  set_target_properties(coreml_proto PROPERTIES COMPILE_FLAGS "-fvisibility-inlines-hidden")
+onnxruntime_add_static_library(coreml_proto ${coreml_proto_srcs})
+target_include_directories(coreml_proto
+                           PUBLIC $<TARGET_PROPERTY:${PROTOBUF_LIB},INTERFACE_INCLUDE_DIRECTORIES>
+                           "${CMAKE_CURRENT_BINARY_DIR}")
+target_compile_definitions(coreml_proto
+                           PUBLIC $<TARGET_PROPERTY:${PROTOBUF_LIB},INTERFACE_COMPILE_DEFINITIONS>)
+set_target_properties(coreml_proto PROPERTIES COMPILE_FLAGS "-fvisibility=hidden")
+set_target_properties(coreml_proto PROPERTIES COMPILE_FLAGS "-fvisibility-inlines-hidden")
 
-  set(_src_sub_dir "coreml_proto/")
-  onnxruntime_protobuf_generate(
-    APPEND_PATH
-    GEN_SRC_SUB_DIR ${_src_sub_dir}
-    IMPORT_DIRS ${COREML_PROTO_ROOT}
-    TARGET coreml_proto
+set(_src_sub_dir "coreml_proto/")
+onnxruntime_protobuf_generate(
+  APPEND_PATH
+  GEN_SRC_SUB_DIR ${_src_sub_dir}
+  IMPORT_DIRS ${COREML_PROTO_ROOT}
+  TARGET coreml_proto
+)
+
+if (NOT onnxruntime_BUILD_SHARED_LIB)
+  install(TARGETS coreml_proto
+          ARCHIVE   DESTINATION ${CMAKE_INSTALL_LIBDIR}
+          LIBRARY   DESTINATION ${CMAKE_INSTALL_LIBDIR}
+          RUNTIME   DESTINATION ${CMAKE_INSTALL_BINDIR}
+          FRAMEWORK DESTINATION ${CMAKE_INSTALL_BINDIR}
   )
-
-  if (NOT onnxruntime_BUILD_SHARED_LIB)
-    install(TARGETS coreml_proto
-            ARCHIVE   DESTINATION ${CMAKE_INSTALL_LIBDIR}
-            LIBRARY   DESTINATION ${CMAKE_INSTALL_LIBDIR}
-            RUNTIME   DESTINATION ${CMAKE_INSTALL_BINDIR}
-            FRAMEWORK DESTINATION ${CMAKE_INSTALL_BINDIR}
-    )
-  endif()
-
-  # Add the .proto and generated .cc/.h files to the External/coreml_proto folder in Visual Studio.
-  # Separate source_group for each as the .proto files are in the repo and the .cc/.h files are generated in the build
-  # output directory.
-  set_target_properties(coreml_proto PROPERTIES FOLDER "External")
-  source_group(TREE ${COREML_PROTO_ROOT} PREFIX coreml_proto FILES ${coreml_proto_srcs})
-
-  # filter to the generated .cc/.h files
-  get_target_property(coreml_proto_generated_srcs coreml_proto SOURCES)
-  list(FILTER coreml_proto_generated_srcs INCLUDE REGEX "\.pb\.(h|cc)$")
-  source_group(TREE ${CMAKE_CURRENT_BINARY_DIR} PREFIX coreml_proto_generated FILES ${coreml_proto_generated_srcs})
 endif()
+
+# Add the .proto and generated .cc/.h files to the External/coreml_proto folder in Visual Studio.
+# Separate source_group for each as the .proto files are in the repo and the .cc/.h files are generated in the build
+# output directory.
+set_target_properties(coreml_proto PROPERTIES FOLDER "External")
+source_group(TREE ${COREML_PROTO_ROOT} PREFIX coreml_proto FILES ${coreml_proto_srcs})
+
+# filter to the generated .cc/.h files
+get_target_property(coreml_proto_generated_srcs coreml_proto SOURCES)
+list(FILTER coreml_proto_generated_srcs INCLUDE REGEX "\.pb\.(h|cc)$")
+source_group(TREE ${CMAKE_CURRENT_BINARY_DIR} PREFIX coreml_proto_generated FILES ${coreml_proto_generated_srcs})
 
 # These are shared utils,
 # TODO, move this to a separated lib when used by EPs other than NNAPI and CoreML
@@ -88,13 +84,6 @@ file(GLOB_RECURSE
   "${ONNXRUNTIME_ROOT}/core/providers/coreml/builders/*.h"
   "${ONNXRUNTIME_ROOT}/core/providers/coreml/builders/*.cc"
 )
-
-if (NOT _BUILD_COREML_PROTO)
-  list(REMOVE_ITEM onnxruntime_providers_coreml_cc_srcs_nested
-    "${ONNXRUNTIME_ROOT}/core/providers/coreml/builders/model_builder.h"
-    "${ONNXRUNTIME_ROOT}/core/providers/coreml/builders/model_builder.cc"
-  )
-endif()
 
 if (_BUILD_COREMLTOOLS)
   # Add helpers to create mlpackage weights
@@ -183,11 +172,9 @@ onnxruntime_add_include_to_target(onnxruntime_providers_coreml
   safeint_interface
 )
 
-if (_BUILD_COREML_PROTO)
-  onnxruntime_add_include_to_target(onnxruntime_providers_coreml coreml_proto)
-  target_link_libraries(onnxruntime_providers_coreml PRIVATE coreml_proto)
-  add_dependencies(onnxruntime_providers_coreml coreml_proto)
-endif()
+onnxruntime_add_include_to_target(onnxruntime_providers_coreml coreml_proto)
+target_link_libraries(onnxruntime_providers_coreml PRIVATE coreml_proto)
+add_dependencies(onnxruntime_providers_coreml coreml_proto)
 
 if (APPLE)
   target_compile_definitions(onnxruntime_providers_coreml PRIVATE __APPLE__)
