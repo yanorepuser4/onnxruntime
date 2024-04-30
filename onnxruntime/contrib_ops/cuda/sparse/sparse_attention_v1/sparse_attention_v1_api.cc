a0ea2aa9fa7abdb63414bc153d11393657f2a2f2
Tianlei Wu <tlwu@microsoft.com>
2024-04-30 13:35:01 -0700

    update api to add sm

--- onnxruntime/contrib_ops/cuda/sparse/sparse_attention_v1/sparse_attention_v1_api.cc
+++ onnxruntime/contrib_ops/cuda/sparse/sparse_attention_v1/sparse_attention_v1_api.cc
@@ -38,7 +38,7 @@ int get_algo_id(SparseAttentionParams& params) {
   return -1;
 }
 
-bool is_supported_sparse_attention(const cudaDeviceProp& dprops) {
+bool is_supported_device(const cudaDeviceProp& dprops) {
   return dprops.major == 8 || (dprops.major == 7 && dprops.minor == 5);
 }
 
@@ -47,7 +47,6 @@ bool is_supported_sparse_attention(int head_size, int sparse_block_size) {
 }
 
 static std::once_flag load_sparse_attention_sm75_fp16_flag;
-static std::once_flag load_sparse_attention_sm75_bf16_flag;
 static std::once_flag load_sparse_attention_sm80_fp16_flag;
 static std::once_flag load_sparse_attention_sm80_bf16_flag;
 
@@ -60,12 +59,11 @@ void load_sparse_attention_fp16(int sm) {
   }
 }
 
-void load_sparse_attention_bf16(int sm) {
-  if (sm == 75) {
-    std::call_once(load_sparse_attention_sm75_bf16_flag, load_sparse_attention_bf16_sm75);
-  } else {
-    assert(sm == 80 || sm == 86 || sm == 89);
+void load_sparse_attention_bf16([[maybe_unused]] int sm) {
+  if (sm == 80 || sm == 86 || sm == 89) {
     std::call_once(load_sparse_attention_sm80_bf16_flag, load_sparse_attention_bf16_sm80);
+  } else {
+    assert(false);
   }
 }
 
@@ -75,12 +73,9 @@ Status run_sparse_attention_bf16(SparseAttentionParams& params) {
     return ORT_MAKE_STATUS(ONNXRUNTIME, FAIL, "no algo found for the parameters");
   }
 
-  if (params.sm == 75) {
-    return sparse_attention_bf16_sm75(params, algo_id);
-  } else {
-    assert(params.sm == 80 || params.sm == 86 || params.sm == 89);
-    return sparse_attention_bf16_sm80(params, algo_id);
-  }
+  // bfloat16 requires sm >= 80
+  assert(params.sm == 80 || params.sm == 86 || params.sm == 89);
+  return sparse_attention_bf16_sm80(params, algo_id);
 }
 
 Status run_sparse_attention_fp16(SparseAttentionParams& params) {
